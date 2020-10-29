4fabb9e6095db73b98fe53edec8ecd3c248a12f5
Tony Melek <tonymelek.au@gmail.com>
2020-10-30 00:14:29 +1100

    update seed.sql

--- db/schema.sql
+++ db/schema.sql
@@ -2,12 +2,13 @@ drop database if exists employee_tracker_db;
 create database employee_tracker_db;
 use employee_tracker_db;
 
-
+-- department table
 create table department (
 id int auto_increment not null,
 name varchar(30),
 primary key(id));
 
+-- role table
 create table role (
 id int auto_increment not null,
 title varchar(30),
@@ -15,6 +16,8 @@ salary decimal(10,2),
 department_id int,
 foreign key (department_id) references department(id),
 primary key(id));
+
+-- employee table
 create table employee (
 id int auto_increment not null,
 first_name varchar(30),
